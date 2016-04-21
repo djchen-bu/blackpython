500f9c0fff42a961636d54a8e9b5f4f1f72de744
djchen-bu <djchen@bu.edu>
2016-04-21 15:15:04 -0400

    added classes
    probably need more

--- blackjack.py
+++ blackjack.py
@@ -5,19 +5,22 @@ Created on Thu Apr 21 14:28:07 2016
 @author: ASS
 """
 
-class deck():
+class gameplay():
     def __init__(self,deck = 4):
         klist = ['d']*13+['c']*13+['h']*13+['s']*13
         number = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']*4
         keys = list(zip(klist,number))
         values = [deck]*52
         ddict = dict(zip(keys,values))
-    def deal()
+        
+    def hit(self):
+        
+    def stand(self):
+        
+    def check(self):
+        
 
 class dealer(deck):
     print('ass')
 class player(deck):
     print('assbeef')
-    
-player1 = player()
-player1.check
