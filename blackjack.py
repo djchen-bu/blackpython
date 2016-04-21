68b275f4d56ba86410c4af5c8b36379b14145189
djchen-bu <djchen@bu.edu>
2016-04-21 15:01:00 -0400

    blackjack classes
    first

---
+++ blackjack.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 21 14:28:07 2016
+
+@author: ASS
+"""
+
+class deck():
+    def __init__(self,deck = 4):
+        klist = ['d']*13+['c']*13+['h']*13+['s']*13
+        number = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']*4
+        keys = list(zip(klist,number))
+        values = [deck]*52
+        ddict = dict(zip(keys,values))
+    def deal()
+
+class dealer(deck):
+    print('ass')
+class player(deck):
+    print('assbeef')
+    
+player1 = player()
+player1.check
