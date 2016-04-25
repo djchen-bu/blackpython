18201eb45b7d9a2f205d8a1f1ea756e4b5f47657
djchen-bu <djchen@bu.edu>
2016-04-25 03:00:23 -0400

    added rest of logic
    i should work but i run into error where it says 'cards' object does not
    support indexing which i think is because its messing with tuples

--- blackjack.py
+++ blackjack.py
@@ -4,6 +4,7 @@ Created on Thu Apr 21 14:28:07 2016
 
 @author: ASS
 """
+import sys
 import random
 from datetime import datetime
 
@@ -15,10 +16,9 @@ class cards():
         values = [4]*52
         ddict = dict(zip(keys,values))
         self.ddict = ddict
-
     def deal(self): #hands out card
         random.seed(datetime.now()) #generates random seed based on time
-        keylist = self.ddict.keys()
+        keylist = list(self.ddict.keys())
         rip = random.sample(keylist,1)
         cards.remove(self,rip)
         return rip
@@ -26,71 +26,78 @@ class cards():
         total = 0
         acecheck = False
         for i in hand:
-            if i[1] in ("J","Q","K"):
+            if i[0][1] == 'J' or i[0][1] == 'Q' or i[0][1] == 'K':
                 total += 10
-            elif i[1] == "A":
+            elif i[0][1] == "A":
                 total += 11
                 acecheck = True
             else:
-                total += int(i[1])
+                total += int(i[0][1])
         if acecheck == True and total > 21 or total == 17:
             total -= 10
         return total
             
     def checkdeck(self):    #checks how many cards are left
-        print(sum(self.ddict.values()))
+        return(sum(self.ddict.values()))
     def remove(self,victim):   #removes cards from dictionary at the end of round
         self.ddict[victim[0]] -= 1
         if self.ddict[victim[0]] == 0:
             del self.ddict[victim[0]]
         
-class dealer(): #defines dealer logic
-    def __init__(self):
-        d = cards()
-        self.dhand = [d.deal(),d.deal()]
-    def check(self, thold = 17):
-        total = cards.checkhand(self,self.dhand)
+class dealer(cards): #defines dealer logic
+    def __init__(self,ddeal):
+        self.ddeal = ddeal
+        self.dhand = [self.ddeal,self.ddeal]
+    def check(self,ddeal, hand, thold = 17):
+        total = hand #self.d.checkhand(self.dhand)
         if total > 21:
-            return False
+            return(0)
         elif total < thold:
-            self.dhand.append(cards.deal())
-            return("hit")
+            self.dhand.append(ddeal)
+            self.check(ddeal,hand)
         else:
-            return True
+            return(total)
     
-class player(): #defines player logic
-    def __init__(self):
-        p = cards()
-        self.phand = [p.deal(),p.deal()]
-    def check(self,thold = 17):
-        total = cards.checkhand(self,self.phand)
-        if total > 21:
-            return False
+class player(cards): #defines player logic
+    def __init__(self,ddeal):
+        self.ddeal = ddeal
+        self.phand = [self.ddeal,self.ddeal]
+    def check(self,ddeal,hand,thold = 17):
+        total = hand #self.p.checkhand(self.phand)
+        if total > 21 or total == 0:
+            return(0)
         elif total < thold:
-            self.phand.append(cards.deal())
-            return("hit")
+            self.phand.append(ddeal)
+            self.check(ddeal,hand)
         else:
-            return True
-class gameplay():
+            return(total)
+class gameplay(player,dealer):
     def __init__(self):
-        self.g = cards()
-        self.g.start()
-        self.d = dealer()
-        self.p = player()
-    def checkplayer(self):
-        chk = self.p.check()
-        if chk == "hit":
-            gameplay.checkplayer()
-        elif chk == False:
-            self.phand = {('0','0'):0}
-            return False
-        elif chk == True:
-            return True
-        print(self.g.checkhand(self.phand))
-
+        self.c = cards()
+        self.c.start()
+        self.pscore = 0
+        self.dscore = 0
+        self.total = 0
+    def dealing(self):
+        if self.c.checkdeck() < 10:
+            sys.exit(self.pscore/self.total,self.dscore/self.total)
+    def checkwinner(self,p,d):
+        if p.check(self.c.deal(),self.c.checkhand(p.phand)) > d.check(self.c.deal(),self.c.checkhand(d.dhand)):
+            self.pscore += 1
+            self.total += 1
+        elif p.check(self.c.deal(),self.c.checkhand(p.phand)) < d.check(self.c.deal(),self.c.checkhand(d.dhand)):
+            self.dscore += 1
+            self.total += 1
+        else:
+            self.total += 1
 
 #    def scoreboard(self, dealerscore = 0, playerscore = 0):
 #        
 #        
 a = gameplay()
-a.checkplayer()
+c = cards()
+while True:
+    a.dealing()
+    d = dealer(c)
+    p = player(c)
+    a.checkwinner(p,d)
